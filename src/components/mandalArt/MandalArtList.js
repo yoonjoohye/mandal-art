0ba55d1d3c91bd90a944a0c2d39a21f9609db0c6
yoonjoohye <67yjh00@gmail.com>
2020-02-02 23:58:01 +0900

    수정

--- src/components/mandalArt/MandalArtList.js
+++ src/components/mandalArt/MandalArtList.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import * as firebase from "firebase";
+import {Map,List} from 'immutable';
 
 import {Link} from 'react-router-dom';
 
@@ -6,21 +8,40 @@ class MandalArtList extends Component {
 
     constructor(props){
         super(props);
-        this.state={
-            list:['1월 인생계획','2월','언젠']
+        this.state= {
+            list: []
         }
+
     }
 
-    render() {
+    componentWillMount() {
+        let uid=JSON.parse(localStorage.getItem('logInfo')).user.uid;
+
+        let database=firebase.database();
+
+        database.ref(`/mandal/${uid}`).once('value').then((snapshot)=> {
+            console.log(snapshot);
+            this.setState({
+                list:[
+                    snapshot.val()
+                    ]
+            });
+        });
+    }
+
+    render(){
+            console.log(this.state.list);
         return (
             <section className="mandal-section">
                     마이페이지
                     {
-                        this.state.list.map((data,index)=>{
-                            return(
-                                <Link to={`/mandal/${index+1}`} key={index} className="mandal-list">{data}</Link>
-                            );
-                        })
+
+                        // this.state.list
+                        // this.state.list.map((data,index)=>{
+                        //     return(
+                        //         <Link to={`/mandal/${index+1}`} key={index} className="mandal-list">{index}</Link>
+                        //     );
+                        // })
                     }
             </section>
         )
