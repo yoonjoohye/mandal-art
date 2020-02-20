6ab796417f176f286acd6c40fa1f90416c7ef1d7
yoonjoohye <67yjh00@gmail.com>
2020-02-21 04:04:04 +0900

    Updates

---
+++ precache-manifest.6f0d7ac56ccb6e80435914be6f2c5b36.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd235941e5e37fcb4a1d117789bc27a",
+    "url": "/mandal-art/index.html"
+  },
+  {
+    "revision": "c886c8c0cdcaa5aba1a7",
+    "url": "/mandal-art/static/css/main.03933d82.chunk.css"
+  },
+  {
+    "revision": "993a07dde761ae691811",
+    "url": "/mandal-art/static/js/2.caa158b0.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/mandal-art/static/js/2.caa158b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c886c8c0cdcaa5aba1a7",
+    "url": "/mandal-art/static/js/main.819e46eb.chunk.js"
+  },
+  {
+    "revision": "a654d50df3987908a7da",
+    "url": "/mandal-art/static/js/runtime-main.7967a694.js"
+  },
+  {
+    "revision": "243d233b63e775dcef6f459428f215f5",
+    "url": "/mandal-art/static/media/index.243d233b.jpg"
+  },
+  {
+    "revision": "f4832b0339425eed66ad14e93b61cc75",
+    "url": "/mandal-art/static/media/mandal-otani.f4832b03.png"
+  },
+  {
+    "revision": "8169e27a6ce3211393b9678c8af1e7a5",
+    "url": "/mandal-art/static/media/mandal.8169e27a.png"
+  },
+  {
+    "revision": "d4ff3ff108150a21e5c5a598a19bd6f7",
+    "url": "/mandal-art/static/media/nothing.d4ff3ff1.jpeg"
+  },
+  {
+    "revision": "bfeb0da3c609c7a7500aa3d26c1664ac",
+    "url": "/mandal-art/static/media/puzzle.bfeb0da3.svg"
+  }
+]);
