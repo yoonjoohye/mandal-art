768ffba7a6ede22b1b956a1e16ad6d9eef91b435
yoonjoohye <67yjh00@gmail.com>
2020-03-20 00:17:28 +0900

    [fix] css 경로 수정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import App from './App';
-import 'css/_Style.scss';
+import './css/_Style.scss';
 
 import firebaseApp from "./firebaseApp";
 // import "firebase/auth";
