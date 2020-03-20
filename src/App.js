d1a5c4e335fa147506a74a0157e7be4cd7cb86c0
yoonjoohye <67yjh00@gmail.com>
2020-03-20 22:40:12 +0900

    [add] 개인정보방침 및 서비스이용약관 추가

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Home from './pages/home/Home';
 import Login from './pages/auth/Login.js';
 import Logout from './pages/auth/Logout.js';
 import Header from './components/header/Header';
-// import Guide from './pages/guide/Guide';
 import Write from "./pages/mandal/write/Index";
 import Detail from "./pages/mandal/detail/Index";
 import Mypage from "./pages/mandal/mypage/Index";
 import NotFound from './pages/NotFound';
+
+import ServiceUseRule from "./pages/auth/ServiceUseRule";
+import PrivacyRule from "./pages/auth/PrivacyRule";
 
 // Redux 관련 불러오기
 import {Provider} from 'react-redux';
@@ -34,6 +36,9 @@ class App extends Component {
                         <Route exact path="/" component={Home}/>
                         <Route exact path="/login" component={Login}/>
                         <Route exact path="/logout" component={Logout}/>
+                        <Route exact path="/service-rule" component={ServiceUseRule}/>
+                        <Route exact path="/privacy-rule" component={PrivacyRule}/>
+
                         <Route path="/write" component={Write}/>
                         <Route path="/mypage" component={Mypage}/>
                         <Route path="/detail/:id" component={Detail}/>
