b1a0f3ab5336d0f3a6f118c809b9a245c58ff10d
yoonjoohye <67yjh00@gmail.com>
2020-05-24 23:46:53 +0900

    [fix]

---
+++ src/components/button/Write.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+
+const Write = (props) => {
+    return(
+        <Link to={'/write'}>
+            <button className="btn write font-white">시작하기</button>
+        </Link>
+    );
+}
+
+
+export default Write;
