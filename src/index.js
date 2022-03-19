2d82493e233e6acab3fd2b57ed82eba3067564a6
yoonjoohye <67yjh00@gmail.com>
2022-03-19 19:41:11 +0900

    chore(firebase)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-// firebase
-import './firebaseApp';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
